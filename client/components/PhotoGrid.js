876d260d2bfa73fb0ce61decdf78026fa66f6f9d
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2016-06-04 16:06:47 -0500

    Show photo grid in main component

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import Photo from './Photo';
+
 const PhotoGrid = React.createClass({
     render() {
         return (
             <div className="photo-grid">
-                I'm in the photo grid
+                {this.props.posts.map((post, index) => {
+                    return (
+                        <Photo key={post.code}
+                            index={index}
+                            post={post}
+                            {...this.props} />
+                    );
+                })}
             </div>
         );
     }
