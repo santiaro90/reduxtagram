b35695ce3be70e83ca8d2fee354eb2a5bdc3b380
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2016-06-04 15:26:51 -0500

    Include app store

---
+++ client/reducers/comments.js
@@ -0,0 +1,4 @@
+export default function comments(state = [], action) {
+    console.log('the comments will change');
+    return state;
+}
---
+++ client/reducers/posts.js
@@ -0,0 +1,4 @@
+export default function posts(state = [], action) {
+    console.log('the posts will change');
+    return state;
+}
